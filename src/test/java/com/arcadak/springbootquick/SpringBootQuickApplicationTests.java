ae8f87563dcbaf821f5677d855fe82330c2eff27
geekbrains <piknik0711@yandex.ru>
2022-12-12 23:37:10 +0300

    Initial commit

---
+++ src/test/java/com/arcadak/springbootquick/SpringBootQuickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arcadak.springbootquick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootQuickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
